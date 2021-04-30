28bd7424f523f964b106be49c291e9e5a536f8ec
wyt <2428282291@qq.com>
2021-04-30 11:14:55 +0800

    feat -> 添加地图的定位和导航等功能

---
+++ pages/map/map.js
@@ -0,0 +1,200 @@
+// pages/map/map.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 地图组件配置
+    setting: {
+      skew: 0,
+      rotate: 0,
+      showLocation: false,
+      showScale: false,
+      subKey: '',
+      layerStyle: 1,
+      enableZoom: true,
+      enableScroll: true,
+      enableRotate: false,
+      showCompass: false,
+      enable3D: false,
+      enableOverlooking: false,
+      enableSatellite: false,
+      enableTraffic: false,
+    },
+    // 当前定位
+    currentLocation: {
+      latitude: "22.773672",
+      longitude: "113.922775",
+    },
+    // 标记点
+    markers: [{
+      id: 0,
+      latitude: "22.773672",
+      longitude: "113.922775",
+      callout: {
+        content: '当前定位点',
+        display: 'ALWAYS',
+        textAlign: "center",
+        fontSize: 14,
+        padding: 5,
+        borderRadius: 5,
+        bgColor: '#f54555'
+      }
+      // iconPath: "/static/images/marker.png"
+    }],
+  },
+  /**
+   * 获取定位
+   */
+  getLocation() {
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.getLocation({
+      // type: 'wgs84',
+      type: 'gcj02',
+      isHighAccuracy: true,
+      success: (res) => {
+        console.log(res);
+        let markers = [...this.data.markers]
+        markers[0].latitude = res.latitude
+        markers[0].longitude = res.longitude
+        this.setData({
+          currentLocation: {
+            ...res
+          },
+          markers: markers,
+        })
+        wx.hideLoading()
+      },
+      fail: (err) => {
+        console.log(err);
+        wx.hideLoading({
+          title: "获取定位失败"
+        })
+      }
+    })
+  },
+  /**
+   * 选择位置
+   */
+  chooseLocation() {
+    wx.chooseLocation({
+      success: (res) => {
+        let markers = [...this.data.markers]
+        let {
+          address,
+          longitude,
+          latitude,
+          name
+        } = res
+        markers.push({
+          id: 1,
+          callout: {
+            content: name,
+            display: 'ALWAYS',
+            textAlign: "center",
+            fontSize: 14,
+            padding: 5,
+            borderRadius: 5
+          },
+          latitude,
+          longitude
+        })
+        this.setData({
+          markers: markers,
+        })
+        console.log(res);
+      }
+    })
+  },
+  /**
+   * 点击地图
+   */
+  tapMap(e) {
+    console.log(e);
+    let {
+      latitude,
+      longitude
+    } = e.detail
+    let markers = [...this.data.markers]
+    markers[1] = {
+      latitude,
+      longitude
+    }
+    this.setData({
+      markers: markers
+    })
+  },
+  /**
+   * 开启路线规划
+   */
+  gotoLocation(){
+    let plugin = requirePlugin('routePlan');
+    let key = '5OXBZ-U5TWP-3VGDJ-LXBUV-FNYUO-MOF4N';  //使用在腾讯位置服务申请的key
+    let referer = 'wechat_demo';   //调用插件的app的名称
+    let endPoint = JSON.stringify({  //终点
+        // 'name': '请输入地址',
+        // 'latitude': 39.89631551,
+        // 'longitude': 116.323459711
+    });
+    wx.navigateTo({
+        url: 'plugin://routePlan/index?key=' + key + '&referer=' + referer + '&endPoint=' + endPoint
+    });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getLocation()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
